a6d785c87aaa424c9666996f2455b73735c161b3
IstalriSkolir <47321091+IstalriSkolir@users.noreply.github.com>
2026-01-30 19:58:33 +0000

    Scripting & Assets
    - Added solid colour materials
    - Created base creature scripts
    - Imported AI navigation package

---
+++ Assets/Scripts/Creatures/Boss.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class Boss : Creature
+{
+    [SerializeField, Header("Boss Gameobjects & Components")]
+    private NavMeshAgent agent;
+    [SerializeField]
+    private Animator animator;
+
+    public virtual void SetDestination(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+
+    public virtual void PlayAnimationByTrigger(string trigger)
+    {
+        animator.SetTrigger(trigger);
+    }
+}
---
+++ Assets/Scripts/Creatures/DevBoss.cs
@@ -0,0 +1,7 @@
+public class DevBoss : Boss
+{
+    internal override void death()
+    {
+        
+    }
+}
