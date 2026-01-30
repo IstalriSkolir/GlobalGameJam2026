d1f279b29fbafc119fb54ceeed94329b3d5e30a7
IstalriSkolir <47321091+IstalriSkolir@users.noreply.github.com>
2026-01-30 20:04:05 +0000

    Removed AI navigation

--- Assets/Scripts/Creatures/Boss.cs
+++ Assets/Scripts/Creatures/Boss.cs
@@ -3,14 +3,14 @@ using UnityEngine.AI;
 
 public abstract class Boss : Creature
 {
-    [SerializeField, Header("Boss Gameobjects & Components")]
-    private NavMeshAgent agent;
+    //[SerializeField, Header("Boss Gameobjects & Components")]
+    //private NavMeshAgent agent;
     [SerializeField]
     private Animator animator;
 
     public virtual void SetDestination(Vector3 position)
     {
-        agent.SetDestination(position);
+        //agent.SetDestination(position);
     }
 
     public virtual void PlayAnimationByTrigger(string trigger)
