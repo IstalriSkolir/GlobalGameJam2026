d5964aab126df405469b1cf50e92a1406be83f88
IstalriSkolir <47321091+IstalriSkolir@users.noreply.github.com>
2026-01-31 16:06:38 +0000

    Added Player Health Regen

--- Assets/Scripts/Creatures/PlayerHealth.cs
+++ Assets/Scripts/Creatures/PlayerHealth.cs
@@ -2,8 +2,52 @@ using UnityEngine;
 
 public class PlayerHealth : Creature
 {
+    [SerializeField, Header("Player Health Properties")]
+    private float regenDelayTime;
+    [SerializeField]
+    private float regenTimer;
+    [SerializeField]
+    private bool regenActive;
+    [SerializeField]
+    private float regenTickDelay;
+    [SerializeField]
+    private float regenTick;
+    [SerializeField]
+    private int regenValue;
+
+    private void Start()
+    {
+        regenTimer = regenDelayTime;
+    }
+
+    private void Update()
+    {
+        if (!regenActive)
+        {
+            if (regenTimer > 0) regenTimer -= Time.deltaTime;
+            else regenActive = true;
+        }
+        else
+        {
+            regenTick -= Time.deltaTime;
+            if (regenTick <= 0)
+            {
+                regenTick = regenTickDelay;
+                UpdateHealthByValue(regenValue, false);
+            }
+        }
+    }
+
     internal override void death()
     {
         
+    }
+
+    public override void UpdateHealthByValue(int change, bool decrease = true)
+    {
+        base.UpdateHealthByValue(change, decrease);
+        regenActive = false;
+        regenTimer = regenDelayTime;
+        regenTick = regenTickDelay;
     }
 }
